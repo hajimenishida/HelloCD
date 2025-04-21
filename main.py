12ad4e5809b1c09be34ab935663befe5e8a9b7b4
hajime <hajime.nishida@gmail.com>
2025-04-21 22:01:29 +0900

    test

---
+++ main.py
@@ -0,0 +1,16 @@
+﻿#
+import tkinter as tk
+
+def say_hello():
+    label.config(text="こんにちは！")
+
+root = tk.Tk()
+root.title("Hello App")
+
+label = tk.Label(root, text="ようこそ")
+label.pack()
+
+button = tk.Button(root, text="挨拶する", command=say_hello)
+button.pack()
+
+root.mainloop()
